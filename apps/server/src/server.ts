57f70d5e8300190cbb048d9a4f52c802020bd3fa
João Victor da Silva <joaovictordasilva0911@gmail.com>
2025-04-23 22:53:07 -0300

    chore(package.json): update main script to "app.js" instead of "index.js" chore(package.json): add build and start scripts for building and running the server feat(server.ts): add error handling and change server listen to async function

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,5 +1,13 @@
 import app from "./app";
 
-app.listen({ port: 3333 }, () => {
-  console.log("🚀 Server listening on http://localhost:3000");
-});
+async function start() {
+  try {
+    await app.listen({ port: 3333, host: '0.0.0.0' });
+    console.log("🚀 Server listening on http://localhost:3333");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+start();
